39dc062e4f942cc0f8854da668f8756d5fd664a0
Rainer Hind <1928782+rai-hi@users.noreply.github.com>
2019-07-15 03:39:04 +0100

    Add grid padder & tests

---
+++ app/models/grid_padder.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+# Class to handle the adding and removing of padding of Grids
+# TODO: would be better to use the actual grid.width/height throughout instead of nested_array.size
+# FIXME: can delete depadder after committing whats here already potentially
+class GridPadder
+  def initialize(pad_value:, pad_width:, pad_height:)
+    @pad_value = pad_value
+    @pad_width = pad_width
+    @pad_height = pad_height
+  end
+
+  def pad(grid)
+    grid_values = grid.clone.values
+
+    padded_horizontally = pad_horizontally grid_values
+    padded_both_directions = pad_vertically padded_horizontally
+
+    Grid.new padded_both_directions
+  end
+
+  def depad(grid)
+    values = grid.clone.values
+
+    depadded_horizontally = depad_horizontally(values)
+    depadded_both_directions = depad_vertically(depadded_horizontally)
+
+    Grid.new depadded_both_directions
+  end
+
+  private
+
+  def pad_horizontally(nested_array)
+    horizontal_padding = [@pad_value] * @pad_width
+
+    nested_array.map do |row|
+      horizontal_padding + row + horizontal_padding
+    end
+  end
+
+  def pad_vertically(nested_array)
+    padding_row = [@pad_value] * nested_array[0].size
+    padding_rows = [padding_row] * @pad_height
+    padding_rows + nested_array + padding_rows
+  end
+
+  def depad_horizontally(nested_array)
+    horizontal_start_index = @pad_width
+    horizontal_end_index = nested_array[0].size - @pad_width - 1
+
+    nested_array.map do |row|
+      row[horizontal_start_index..horizontal_end_index]
+    end
+  end
+
+  def depad_vertically(nested_array)
+    vertical_start_index = @pad_height
+    vertical_end_index = nested_array.size - @pad_height - 1
+
+    nested_array[vertical_start_index..vertical_end_index]
+  end
+end
---
+++ test/models/grid_padder_test.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# TODO: re-order for clarity
+class GridPadderTest < MiniTest::Test
+  def setup
+    @non_padded_values = [
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1]
+    ]
+
+    @non_padded_grid = Grid.new @non_padded_values
+
+    @pad_height = 5
+    @pad_width = 3
+    @pad_value = 0
+
+    @padder = GridPadder.new(
+      pad_value: @pad_value, pad_width: @pad_width, pad_height: @pad_height
+    )
+
+    @padded = @padder.pad(@non_padded_grid)
+    @depadded = @padder.depad(@padded)
+  end
+
+  def test_original_grid_is_unmodified_after_pad
+    original = RandomBinaryGrid.generate
+    original_values = original.values.clone
+    @padder.pad(original)
+    assert_equal original_values, original.values
+  end
+
+  def test_padded_grid_is_correct_height
+    expected = @non_padded_grid.height + (2 * @pad_height)
+    assert_equal expected, @padded.height
+  end
+
+  def test_padded_grid_is_correct_width
+    expected = @non_padded_grid.width + (2 * @pad_width)
+    assert_equal expected, @padded.width
+  end
+
+  def test_original_grid_is_unmodified_after_depadding
+    original = RandomBinaryGrid.generate
+    original_values = original.values.clone
+    @padder.depad(original)
+    assert_equal original_values, original.values
+  end
+
+  def test_pad_depad_returns_starting_values
+    assert_equal @non_padded_grid.values, @depadded.values
+  end
+
+  def test_padded_grid_still_valid
+    assert @padded.valid?
+  end
+
+  def test_depadded_grid_still_valid
+    assert @depadded.valid?
+  end
+
+  def test_horizontal_padding_values
+    padding_area = @padded.values.map do |row|
+      row[0..@pad_width - 1]
+    end
+
+    assert_equal [@pad_value], padding_area.flatten.uniq
+  end
+
+  def test_vertical_padding_values
+    top_padding_area = @padded.values[0..@pad_height - 1]
+    bottom_padding_area = @padded.values[-@pad_height..-1]
+    all_padding_rows = top_padding_area + bottom_padding_area
+
+    assert_equal [@pad_value], all_padding_rows.flatten.uniq
+  end
+end
